6baab3fa9d0e5c0a631cb91000e68d83d870f154
FelixChirinosLopez <felixchirinoslopez@gmail.com>
2022-08-15 01:45:39 +0100

    first

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,67 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+
+    "data": [
+
+      {
+
+        "name": "Han Solo",
+
+        "email": "han@shot.first"
+
+      },
+
+      {
+
+        "name": "Robocop",
+
+        "email": "robocop@omnicorp.com"
+
+      },
+
+      {
+
+        "name": "Danny Devito",
+
+        "email": "devito@paddys.com"
+
+      },
+
+      {
+
+        "name": "Arya Stark",
+
+        "email": "faceless@man"
+
+      },
+
+      {
+
+        "name": "John McClane",
+
+        "email": "mcclane@nakatomi.plaza"
+
+      },
+
+      {
+
+        "name": "Indiana Jones",
+
+        "email": "indiana@explorers.org"
+
+      },
+
+      {
+
+        "name": "Alex Etel",
+
+        "email": "alex@builtbypixel.com"
+
+      }
+
+    ]
+
+  })
 }
